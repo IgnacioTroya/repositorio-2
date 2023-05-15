ea00e880885c749bb4a84bd096eec93b69650f97
IgnacioTroya <ignaciotroya175212@gmail.com>
2023-05-15 10:28:34 -0500

    Primer codigo en clase

---
+++ Animales.java
@@ -0,0 +1,11 @@
+public class Animales {
+    int edad; 
+    String nombre;
+    public Animales(int edad, String nombre) {
+        this.edad = edad;
+        this.nombre = nombre;
+    }
+public void comer (String animal){
+    System.out.println("La "+animal+ " esta comiendo pichula");
+}    
+}
--- main.java
+++ main.java
@@ -1,5 +1,10 @@
-public class Main  {
-    public static void Main(string[]args) {   
-        System.out.println("hola mundo")
-    }
+public class Main {
+public static void main(String[] args) {
+    Animales animal1 = new Animales(5, "Ramon");
+    System.out.println("El nombre del animal 1 es"+animal1.nombre);
+    System.out.println("La edad del animal es:"+animal1.edad);
+    animal1.comer("Zorra");
+} 
+    
+
 }
